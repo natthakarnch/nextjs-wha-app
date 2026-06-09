39f38f6f8caf00e57abdef23cb14890293e6b668
Natthakarn Chaiyotmanon <natthakarnch@wha-group.com>
2026-06-09 10:37:00 +0700

    refactor: move course fetch logic to service layer

--- src/app/(front)/course/page.tsx
+++ src/app/(front)/course/page.tsx
@@ -1,15 +1,12 @@
 import FeaturesCourse from "@/components/features-course";
+import { getCourses } from "@/services/course.service";
 
 // http://localhost:3000/course
 export default async function CoursePage() {
-  const response = await fetch('https://api.codingthailand.com/api/course');
-  const courseResponse = await response.json();
+  const courseResponse = await getCourses();
 
   return (
     <main>
-      {/* {
-        JSON.stringify(courseResponse.data)
-      } */}
       {
         courseResponse.data.length > 0 && <FeaturesCourse courses={courseResponse.data} />
       }
