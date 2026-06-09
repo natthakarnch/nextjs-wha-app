7fc777e2f27d3b56e6bd32fe1362bcfe4df88160
Natthakarn Chaiyotmanon <natthakarnch@wha-group.com>
2026-06-09 15:34:32 +0700

    update bash permissions and uncomment hello world in home page

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/hero";
 export default function Home() {
   return (
     <div className="p-8">
-      {/* <h1 className="text-4xl font-bold mb-4 text-center">Hello Word!</h1> */}
+      <h1 className="text-4xl font-bold mb-4 text-center">Hello Word!</h1>
       <Hero />
     </div>
   );
